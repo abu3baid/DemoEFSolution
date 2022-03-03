73c9e581d38b7b136392b21fb23a09b7c741c32a
abu3beid <abu3beid@X570>
2022-03-04 00:41:41 +0200

    Add project files.

---
+++ DemoEFApplication/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoEFApplication.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
---
+++ DemoEFApplication/Models/Teacher.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoEFApplication.Models
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string TeacherName { get; set; }
+        public int TeacherAge { get; set; }
+    }
+}
